d4d358f627f2f964c9caf5e13f1729135d84a194
Benoit Chauvet <contact@benoitchauvet.com>
2023-07-20 16:58:14 +0200

    day 4-1

--- advent/day3.py
+++ advent/day3.py
@@ -19,31 +19,94 @@ def part1():
     
     gamma = 0
     epsilon = 0
-    bytes_count = len(ones)
+    nb_bits = len(ones)
 
-    for i in range(1, bytes_count+1):
-        value = ones[i-1]
+    for i in range(1, nb_bits + 1):
+        value = ones[i - 1]
         if value > nb_lines - value:
-            gamma += pow(2,bytes_count - i)
+            gamma += pow(2,nb_bits - i)
         else:
-            epsilon += pow(2,bytes_count - i)
+            epsilon += pow(2,nb_bits - i)
 
     print(gamma*epsilon)
+
+def get_remaining_number_oxygen(lines, column):
+     
+    remain = list()
+    nb_lines = len(lines)
+    nb_ones = 0
+    nb_zeros = 0
+
+    for line in lines:
+        if line[column] == '1':
+            nb_ones += 1
+        else:
+            nb_zeros += 1 
+
+    for line in lines:
+        value = line[column]
+        if value == '1' and nb_ones >= nb_zeros:
+            remain.append(line)
+        if value == '0' and nb_ones < nb_zeros:
+            remain.append(line)
+
+    if len(remain) == 1:
+        return remain[0]
+    else:
+        return get_remaining_number_oxygen(remain, column + 1)
+
+def get_remaining_number_co2(lines, column):
+     
+    remain = list()
+    nb_lines = len(lines)
+    nb_ones = 0
+    nb_zeros = 0
+
+    for line in lines:
+        if line[column] == '1':
+            nb_ones += 1
+        else:
+            nb_zeros += 1 
+
+    for line in lines:
+        value = line[column]
+        if value == '1' and nb_ones < nb_zeros:
+            remain.append(line)
+        if value == '0' and nb_ones >= nb_zeros:
+            remain.append(line)
+
+    if len(remain) == 1:
+        return remain[0]
+    else:
+        return get_remaining_number_co2(remain, column + 1)
+
+def binary_to_int(value):
     
+    result = 0
+    nb_bits = len(value)
+
+    for i in range(1, nb_bits + 1):
+        bit = value[i - 1]
+        if bit == '1':
+            result += pow(2,nb_bits - i)
+
+    return result
 
 def part2():
 
     lines = file_utils.get_lines(path)
-    nb_lines = len(lines)
 
-    ones = compute_ones(lines)
+    nb_bytes = len(lines[0])
+   
+    oxygen = get_remaining_number_oxygen(lines, 0)
+    co2 = get_remaining_number_co2(lines, 0)
 
-    for i in range(1, bytes_count+1):
-        value = ones[i-1]
-        if value > nb_lines - value:
-            gamma += pow(2,bytes_count - i)
-        else:
-            epsilon += pow(2,bytes_count - i)
+    # print(oxygen)
+    # print(binary_to_int(oxygen))
+    # print(co2)
+    # print(binary_to_int(co2))
+
+    print(binary_to_int(oxygen) * binary_to_int(co2))
 
 if __name__ == "__main__":
     part1()
---
+++ advent/day4.py
@@ -0,0 +1,86 @@
+import file_utils
+import array as arr
+
+board_size = 5
+
+path = "./data/day4.txt"
+
+numbers = arr.array('i')
+boards = list()
+
+def init_game():
+    f = open(path, "r")
+
+    # init numbers:
+    line = f.readline()
+    line_split = line.strip().split(',')
+    
+    for item in line_split:
+        numbers.append(int(item))
+
+    # init boards
+    while line != '':
+        line = f.readline()
+
+        board = [[]]
+        
+        if line.strip() != '':
+            for row in range(0,5): 
+                line_split = line.replace('  ', ' ').strip().split(' ')
+                board.append([])
+                for i in range(0, len(line_split)):
+                    item = line_split[i]
+                    board[row].append(int(item))
+                line = f.readline()
+            board.pop()
+            boards.append(board)
+
+    f.close()
+
+def mark_board(board, number):
+    for i in range(0,board_size):
+        for j in range(0,board_size):
+            if board[i][j] == number:
+                board[i][j] = -1
+                return
+
+def check_win(board):
+    for i in range(0,board_size):
+        nb_x = 0
+        nb_y = 0
+        for j in range(0,board_size):
+            if board[i][j] == -1: 
+                nb_x +=1
+            if board[j][i] == -1: 
+                nb_y +=1
+        if (nb_x == board_size or nb_y == board_size):
+            return True
+    return False
+
+def caclulate_score(board, last_number):
+    score = 0
+    for i in range(0, board_size):
+        for j in range(0, board_size):
+            if board[i][j] > -1:
+                score += board[i][j]
+    return score * last_number
+
+def part1():
+
+    init_game()
+    for number in numbers:
+        for board in boards:
+            mark_board(board, number)
+            if check_win(board):
+                print(caclulate_score(board, number))
+                return
+
+
+def part2():
+
+    print("ok")
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
